067c9f1d2f8523cd765bf39bfdd700c248f2f5ff
Khoa Pham <onmyway133@gmail.com>
2016-10-09 13:35:45 +0200

    Get node children

--- Sources/Element.swift
+++ Sources/Element.swift
@@ -46,4 +46,44 @@ class Element {
   lazy var content: String? = {
     return xmlNodeGetContent(self.xmlNode).toString()
   }()
+
+  lazy var nextSibing: Element = {
+    return Element(node: self.xmlNode.pointee.next, document: self.xmlDocument)
+  }()
+
+  lazy var previousSibing: Element = {
+    return Element(node: self.xmlNode.pointee.prev, document: self.xmlDocument)
+  }()
+
+  func children(predicate: (Element, Int) -> Bool) -> [Element] {
+    var elements = [Element]()
+    var cursor = self.xmlNode.pointee.children
+    var index = 0
+
+    while cursor != nil {
+      if let cursor = cursor, cursor.pointee.type == XML_ELEMENT_NODE {
+        let element = Element(node: cursor, document: self.xmlDocument)
+        if predicate(element, index) {
+          elements.append(element)
+        }
+      }
+
+      index += 1
+      cursor = cursor?.pointee.next
+    }
+
+    return elements
+  }
+
+  func children(name: String) -> [Element] {
+    return children { element, index in
+      return element.name == name
+    }
+  }
+
+  func children(indexes: [Int]) -> [Element] {
+    return children { element, index in
+      return indexes.contains(index)
+    }
+  }
 }
