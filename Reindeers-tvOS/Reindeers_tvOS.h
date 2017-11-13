f1fe4935c02716c2e36f0192f006466f4af1260c
Khoa Pham <onmyway133@gmail.com>
2017-11-13 11:16:20 +0100

    Add tvOS support

---
+++ Reindeers-tvOS/Reindeers_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Reindeers_tvOS.h
+//  Reindeers-tvOS
+//
+//  Created by Khoa Pham on 13.11.2017.
+//  Copyright © 2017 Khoa Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reindeers_tvOS.
+FOUNDATION_EXPORT double Reindeers_tvOSVersionNumber;
+
+//! Project version string for Reindeers_tvOS.
+FOUNDATION_EXPORT const unsigned char Reindeers_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reindeers_tvOS/PublicHeader.h>
+
+
