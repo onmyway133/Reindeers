0b00ba93528087de33774bb6d55260b0345168cd
Khoa Pham <onmyway133@gmail.com>
2017-11-13 11:37:11 +0100

    Add macOS app

---
+++ Example/ReindeerDemo/ReindeerMacApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReindeerMacApp
+//
+//  Created by Khoa Pham on 13.11.2017.
+//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
